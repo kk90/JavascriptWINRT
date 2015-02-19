34a107872752c9f0a7317db41d80cb7a98548ebe
kk90 <kr.korycki@gmail.com>
2015-02-19 19:21:10 +0100

    init

---
+++ CalendarIPM/jasmine-standalone-2.2.0/spec/calendartests.js
@@ -0,0 +1,50 @@
+﻿describe("Date converter", function () {
+    it("sate conversion", function () {
+        var examplestart = new Date(1995, 11, 17, 3, 24, 0);
+        var exampleend = new Date(1995, 11, 17, 4, 24, 0);
+        
+        var testcalendarobject = {
+            start_date: examplestart,
+            end_date: exampleend,
+            text: "test"
+        }
+
+        var model = toModel(1,testcalendarobject);
+
+        expect(model.start_date).toBe("12.17.1995 03:24");
+        expect(model.end_date).toBe("12.17.1995 04:24");
+        expect(model.text).toBe("test");
+    });
+});
+
+//describe("Data base", function () {
+//    DB.init();
+//    beforeEach(function (done) {
+//        setTimeout(function () {
+            
+//            done();
+//        }, 1000);
+//    });
+
+//    it("saving data", function () {
+        
+   
+//    expect(DB.data.length).toBe(1);
+   
+
+        //var testitem = {
+        //    id: "123",
+        //    text: "123",
+        //    start_date: "12.17.1995 03:24",
+        //    end_date: "12.17.1995 04:24"
+        //}
+
+//        DB.add(testitem);
+
+//        DB.getAll();
+//        expect(DB.data.length).toBe(1);
+
+//        done();
+
+//    });
+//});
---
+++ CalendarIPM/js/indexDBengine.js
@@ -0,0 +1,110 @@
+﻿
+
+DB = (function () {
+    var database = null;
+
+    return {
+        data: [],
+        getAll: function () {
+            DB.data = [];
+            if (database != null) {
+                var objectStore =
+            database.transaction("notes").objectStore("notes");
+                objectStore.openCursor().onsuccess = function (event) {
+
+                    var cursor = event.target.result;
+                    if (cursor) {
+                        DB.data.push(cursor.value)
+                        console.log(cursor.value.id);
+                        cursor.continue();
+                    }
+
+                }
+            };
+        },
+        init: function () {
+            if (indexedDB) {
+                const dbName = "notes_c";
+                const dbVer = 1;
+                var request = indexedDB.open(dbName, dbVer);
+
+                request.onerror = function (event) {
+                };
+
+                request.onupgradeneeded = function (event) {
+                    database = event.target.result;
+                    if (!database.objectStoreNames.contains("notes")) {
+                        var objectStore = database.createObjectStore("notes", {
+                            keyPath: "id",
+                            autoIncrement: false
+                        });
+                    }
+
+                };
+
+                request.onsuccess = function (e) {
+
+                    database = e.target.result;
+                    DB.getAll();
+
+                };
+            }
+
+        },
+        add: function (item) {
+            if (database != null) {
+                var filesObjectStore =
+                  database.transaction("notes", "readwrite").objectStore("notes");
+                console.log("add" + item.id);
+                filesObjectStore.add(item);
+            }
+        },
+        edit: function (item) {
+
+        },
+        remove: function (item) {
+            if (database != null) {
+                var filesObjectStore =
+                  database.transaction("notes", "readwrite").objectStore("notes");
+
+
+                var request = filesObjectStore.delete("" + item.id);
+                request.onsuccess = function (event) {
+                    console.log("removed"+item.id);
+                    DB.getAll();
+                };
+
+                request.onerror = function (event) {
+                    console.log("shit..");
+                    DB.getAll();
+                };
+            }
+        }
+    }
+})();
+
+
+function toModel(id_, object_) {
+    function twoPositionNumber(num) {
+        if (num < 10) {
+            return "0" + num;
+        } else {
+            return num;
+        }
+
+    }
+    function getDate(date) {
+        return twoPositionNumber(date.getMonth() + 1) + '.'
+                + twoPositionNumber(date.getUTCDate()) + '.'
+                + date.getFullYear() + ' '
+                + twoPositionNumber(date.getHours()) + ':'
+                + twoPositionNumber(date.getMinutes());
+    }
+
+    return note = {
+        id: id_,
+        text: object_.text,
+        start_date: getDate(object_.start_date),
+        end_date: getDate(object_.end_date)
+    }
+}
